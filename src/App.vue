066220ad2ca55d6ab5f76fac07a3fa766d565616
mr-zero <amirajubolchi001@gmail.com>
2020-09-18 18:46:41 +0530

    Navbar components added

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,17 @@
 <template>
     <v-app>
-        <v-navigation-drawer color="purple" v-show="sideBar" temporary>
-            <v-list>
-                <v-list-item-icon>
-                    <v-icon>mdi-account-supervisor</v-icon>
-                    Account
-                </v-list-item-icon>
-            </v-list>
-        </v-navigation-drawer>
-        <v-toolbar>
-            <v-app-bar-nav-icon @click="sideBar = !sideBar"></v-app-bar-nav-icon>
-
-            <v-toolbar-title>Dev MeetUp</v-toolbar-title>
-
-            <v-spacer></v-spacer>
-
-            <v-toolbar-items>
-                <v-btn color="purple" elevation="10">
-                    <v-icon large>mdi-account-supervisor</v-icon>
-                    View MeetUp
-                </v-btn>
-            </v-toolbar-items>
-        </v-toolbar>
+    <Navbar />
     </v-app>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
 export default {
     name: "App",
 
-    components: {},
+    components: {
+            Navbar,
+        },
 
     data: () => ({
         sideBar: false,
