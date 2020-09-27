ee3ee1cfd3c8241f40b4e0b93b61b7b9406b8f70
mr-zero <amirajubolchi001@gmail.com>
2020-09-28 03:20:17 +0530

    meetup set up

--- src/views/Meetup/Meetup.vue
+++ src/views/Meetup/Meetup.vue
@@ -4,19 +4,19 @@
             <v-col cols="12">
                 <v-card elevation="5" hover>
                     <v-card-title>
-                        <h3 class="primary--text">My Meetup</h3>
+                        <h3 class="primary--text">{{meetup.title}}</h3>
                     </v-card-title>
                     <v-card-subtitle>
-                        <div class="info--text">In 17 July, 2020 </div>
+                        <div class="info--text">{{meetup.date}}</div>
                     </v-card-subtitle>
-                    <v-img class="" src="https://i.ibb.co/tXx9r0K/science-city.jpg" height="400px"></v-img>
+                    <v-img class="" :src="meetup.imageUrl" height="400px"></v-img>
                     <v-card-text>
-                    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequuntur, temporibus, laudantium cum quos harum veritatis iste, cumque dolor tempore sint! Nam quisquam distinctio, ea repellat fuga facilis in unde.</div>
+                        <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequuntur, temporibus, laudantium cum quos harum veritatis iste, cumque dolor tempore sint! Nam quisquam distinctio, ea repellat fuga facilis in unde.</div>
                     </v-card-text>
-                <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn class="mr-2 info">Register</v-btn>
-                </v-card-actions>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="mr-2 info">Register</v-btn>
+                    </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
@@ -27,5 +27,10 @@
 export default {
     name: "Meetup",
     components: {},
+    computed: {
+            meetup(){
+                    return this.$store.getters.loadedMeetup(this.$route.params.id)
+                }
+        },
 };
 </script>
--- src/views/Meetup/Meetups.vue
+++ src/views/Meetup/Meetups.vue
@@ -1,22 +1,22 @@
 <template>
     <v-container fill-height fill-width fluid>
-        <v-row align="center" justify="center">
+        <v-row align="center" justify="center" v-for="meetup in meetups" :key="meetup.id">
             <v-col cols="12" md="8">
                 <v-card class="info" elevation-6>
                     <v-container fluid>
                         <v-row>
                             <v-col cols="5" sm="4" md="3">
-                                <v-img class="white--text align-end" src="https://i.ibb.co/MCCmYgp/1200px-Lake-Gardens-Kuala-Lumpur-03.jpg" height="125px"></v-img>
+                                <v-img class="white--text align-end" :src="meetup.imageUrl" height="125px"></v-img>
                             </v-col>
                             <v-col cols="7" sm="8" md="9">
                                 <v-card-title>
                                     <div>
-                                        <h5 class="white--text mb-0">Lake Gardens</h5>
-                                        <div>7th July 2020</div>
+                                        <h5 class="white--text mb-0">{{ meetup.title }}</h5>
+                                        <div>{{ meetup.date }}</div>
                                     </div>
                                 </v-card-title>
                                 <v-card-actions>
-                                    <v-btn text>
+                                    <v-btn text :to="{ name: 'Meetup', params: { id: meetup.id } }">
                                         <v-icon left light>mdi-arrow-right</v-icon>
                                         View Meetup
                                     </v-btn>
@@ -34,5 +34,10 @@
 export default {
     name: "Meetups",
     components: {},
+    computed: {
+        meetups() {
+            return this.$store.getters.loadedMeetups;
+        },
+    },
 };
 </script>
